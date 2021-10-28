4c4d945ebb9fda3114b91c0f4fb2c9fef6d57349
Incisivity <89982973+Incisivity@users.noreply.github.com>
2021-10-28 23:29:56 +0200

    YEah
    no

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,62 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<title>Idiotproof Website</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="description" content="Idiotproof Website">
+<meta name="keywords" content="Idiotproof Website">
+<meta name="robots" content="index,follow">
+<link rel="shortcut icon" href="/favicon.ico">
+<style type="text/css">
+body,
+a,
+a:hover{
+	font-family:Verdana, Geneva, Arial;
+	background-color:#FFFFFF;
+	color:#CC3300;
 }
+a img{text-decoration:none;border:0px;}
+#counter{	
+	font-size:11px;
+	text-align:center;
+	color:#cccccc;
+	position:absolute;
+    bottom: 0px;
+	height:40px;
+	width:99%;
+	
+}
+#counter a{color:#dddddd;}
+div#facebook{position:absolute; bottom:15px;text-align:center;width:99%;}
+</style>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-PB2YHZLW19"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PB2YHZLW19');
+</script>
+</head>
+<body data-new-gr-c-s-check-loaded="14.1036.0" data-gr-ext-installed="">
+<center>
+<!-- div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<br/><br/ -->
+<div align="center">
+<p><a href="/"><img src="/logo.gif" name="logo" id="logo" alt="Idiotproof Website" border="0"></a></p>
+<br>
+<form id="form1" name="form1" method="post"><input type="hidden" name="domain" value=""><p><input type="submit" name="submit" value="Click Here"></p></form><p style="padding-top:8px;border:0px;">&nbsp;</p><br></div>
+</center>
+
+
+
+<!--
+Site created on December 4, 2006.
+Now: 
+Thursday 28th of October 2021 04:29:04 PM--></body><grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration></html>
